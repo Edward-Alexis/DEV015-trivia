dab55b1514d63436012e37e98498d70d38f2ee41
Edward <edwardalexisquispequispe083@gmail.com>
2024-09-24 16:04:53 -0500

    falta un cronometro

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-/*
-  Aquí va tu JavaScript
-*/
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById("buttonWelcome");
+
+    button.addEventListener('click', function (event) {
+        event.preventDefault(); // Previene el comportamiento por defecto del botón
+
+        let nombre_jugador = document.getElementById('nombre').value; // Obtener el valor del input
+        console.log("NOMBRE", nombre_jugador); // Imprimir el nombre
+        nombre_jugador = nombre_jugador.toUpperCase(); // Convertir a mayúsculas
+        localStorage.setItem('nombreJugador', nombre_jugador); // Almacenar en localStorage
+
+        change(); // Llamar a la función change
+    });
+
+    function change() {
+        window.location.href = 'escoger.html'; // Redirigir a la nueva página
+    }
+});
+
+function seleccionarNivel(nivel) {
+    if (nivel === 'facil') {
+        window.location.href = 'welcome.html';
+    } else if (nivel === 'dificil') {
+        window.location.href = 'pesadilla.html';
+    }
+}
 /**
  Aqui va el JS de facil
  */
@@ -29,10 +52,10 @@ function corregir_facil() {
 
     document.getElementById('resultado').textContent = aciertos;
 
-    let nombre = "Usuario";
 
     if (aciertos === 5) {
-        alert(`¡Felicitaciones ${nombre} por completar con éxito esta trivia!`);
+        alert(`¡Felicitaciones por completar con éxito esta trivia! Te invito a probar el modo pesadilla ,¿Te Atreves?`);
+        window.location.href = "pesadilla.html";
     } else {
         alert(`${nombre}, has acertado ${aciertos} de 5 preguntas. ¡Inténtalo de nuevo!`);
     }
@@ -41,7 +64,7 @@ function corregir_facil() {
 /**
  Aqui va el JS de pesadilla
  */
- function corregir_dificil() {
+function corregir_dificil() {
     let aciertos = 0;
 
     if (document.querySelector('input[name="p1"]:checked')?.value === '3') {
@@ -67,7 +90,12 @@ function corregir_facil() {
     document.getElementById('resultado').textContent = aciertos;
 
     if (aciertos === 5) {
-        let nombre = prompt("¡Felicitaciones! Por favor, introduce tu nombre:");
-        alert(`¡Felicitaciones ${nombre} por completar con éxito esta trivia!`);
+        let confirmacion = confirm('¡Felicitaciones por completar con éxito esta trivia! ¿Te gustaría volver a intentarlo?');
+        if (confirmacion) {
+            window.location.href = "escoger.html"; // Redirige si hace clic en "Aceptar"
+        } else {
+            // No hace nada si hace clic en "Cancelar"
+            console.log("El usuario canceló.");
+        }
     }
-}
+}    
